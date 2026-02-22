db993b59421573d9911b995cd9f36ccb6aa3912c
BohemianBrunette00 <olivernabais@gmail.com>
2026-02-22 20:34:39 +0100

    WEB - FALTA WASD

---
+++ OLDDOG - WEB/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "LabRats-OLD DOG-0.1.0";
+const contentToCache = [
+    "Build/OLDDOG - WEB.loader.js",
+    "Build/OLDDOG - WEB.framework.js.br",
+    "Build/OLDDOG - WEB.data.br",
+    "Build/OLDDOG - WEB.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
